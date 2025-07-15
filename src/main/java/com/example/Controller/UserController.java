1ef851b6c0e3a4b286ebfe3a64f7a6f7c5743abd
devsharma009 <devanshsharrma.009@gmail.com>
2025-07-15 14:38:32 +0530

    just cheking

--- src/main/java/com/example/Controller/UserController.java
+++ src/main/java/com/example/Controller/UserController.java
@@ -1,4 +1,12 @@
 package com.example.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class UserController {
+
+    @GetMapping("/dev")
+    public int function(){
+        return 123;
+
+    }
 }
